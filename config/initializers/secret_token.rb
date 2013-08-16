fe7d43d5e277123ce45c3330a3044a9b263d6a57
montekaka <josh.chen.jia@gmail.com>
2013-08-15 21:43:30 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinterest::Application.config.secret_token = '20a1321c4b01064b9146b641e3fe1f2ee7b76c6e186e68675be9b09489f30a0c59b14d5e4c3704520d77c8839a6c827827ccd2fd7067af486dbafb56d443f35b'
